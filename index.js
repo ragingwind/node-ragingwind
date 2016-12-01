d7328ec6d0b0630ed4fb3ad753b03f0b932b1474
ragingwind <ragingwind@gmail.com>
2016-12-01 16:45:18 +0900

    Add birthday

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   name: () => { return 'Jimmy Moon' },
   email: () => { return 'ragingwind@gmail.com' },
   github: () => { return 'ragingwind' },
-  twitter: () => { return 'ragingwind' }
+  twitter: () => { return 'ragingwind' },
+  birthday: () => { return '2015-10-30'}
 };
--- test.js
+++ test.js
@@ -4,3 +4,4 @@ console.log(ragingwind.name() === 'Jimmy Moon');
 console.log(ragingwind.email() === 'ragingwind@gmail.com');
 console.log(ragingwind.github() === 'ragingwind');
 console.log(ragingwind.twitter() === 'ragingwind');
+console.log(ragingwind.birthday() === '2015-10-30');
